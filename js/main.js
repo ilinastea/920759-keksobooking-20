d2b31f6501678c5fc8e40faab0f5c71c0272c513
ilinastea <ilinastea@gmail.com>
2020-06-10 22:19:41 +0300

    Добавлены все фото, функции - отдельно

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ var offerTitles = ['Title1', 'Title2', 'Title3', 'Title4', 'Title5', 'Title6', '
 var offerTypes = ['palace', 'flat', 'house', 'bungalo'];
 var offerCheckin = ['12:00', '13:00', '14:00'];
 var offerFeatures = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
-var offerPhotos = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
+var offerPhotos = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg','http://o0.github.io/assets/images/tokyo/hotel2.jpg','http://o0.github.io/assets/images/tokyo/hotel3.jpg','http://o0.github.io/assets/images/tokyo/hotel1.jpg','http://o0.github.io/assets/images/tokyo/hotel2.jpg','http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
 
 //  вспомогательная функция
 var random = function (min, max) {
@@ -30,7 +30,7 @@ var createSimilarNotices = function () {
         checkout: offerCheckin[random(0, offerCheckin.length - 1)],
         features: offerFeatures.slice(0, random(1, offerFeatures.length - 1)),
         description: 'Здесь будет описание',
-        photos: offerPhotos.slice(0, random(1, offerPhotos.length - 1))
+        photos: offerPhotos.slice(0, random(1, offerPhotos.length))
       },
       location: {
         x: random(100, 800),
@@ -43,6 +43,8 @@ var createSimilarNotices = function () {
 };
 
 var notices = createSimilarNotices();
+
+console.log(notices);
 
 var map = document.querySelector('.map');
 map.classList.remove('map--faded');
@@ -72,47 +74,59 @@ var addCard = function (card) {
   newCard.querySelector('.popup__title').textContent = card.offer.title;
   newCard.querySelector('.popup__text--address').textContent = card.offer.adress;
   newCard.querySelector('.popup__text--price').textContent = card.offer.price + '₽/ночь';
-  if (card.offer.type === 'palace') {
-    newCard.querySelector('.popup__type').textContent = 'Дворец';
-  }
-  if (card.offer.type === 'flat') {
-    newCard.querySelector('.popup__type').textContent = 'Квартира';
-  }
-  if (card.offer.type === 'house') {
-    newCard.querySelector('.popup__type').textContent = 'Дом';
-  }
-  if (card.offer.type === 'bungalo') {
-    newCard.querySelector('.popup__type').textContent = 'Бунгало';
-  }
   newCard.querySelector('.popup__text--time').textContent = 'Заезд после ' + card.offer.checkin + ', выезд до ' + card.offer.checkout;
-
-  var features = newCard.querySelector('.popup__features');
-  if (!card.offer.features.includes('wifi')) {
-    features.removeChild(newCard.querySelector('.popup__feature--wifi'));
-  }
-  if (!card.offer.features.includes('dishwasher')) {
-    features.removeChild(newCard.querySelector('.popup__feature--dishwasher'));
-  }
-  if (!card.offer.features.includes('parking')) {
-    features.removeChild(newCard.querySelector('.popup__feature--parking'));
-  }
-  if (!card.offer.features.includes('washer')) {
-    features.removeChild(newCard.querySelector('.popup__feature--washer'));
-  }
-  if (!card.offer.features.includes('elevator')) {
-    features.removeChild(newCard.querySelector('.popup__feature--elevator'));
-  }
-  if (!card.offer.features.includes('conditioner')) {
-    features.removeChild(newCard.querySelector('.popup__feature--conditioner'));
-  }
-
   newCard.querySelector('.popup__description').textContent = card.offer.description;
-
-  var photos = newCard.querySelector('.popup__photos');
-  photos.querySelector('img').src = card.offer.photos[0];
-
   map.insertBefore(newCard, document.querySelector('.map__filters-container'));
   return newCard;
 };
+var popup = addCard(notices[0]);
 
-addCard(notices[0]);
+var addCardPhotos = function (card) {
+  var photos = popup.querySelector('.popup__photos');
+  var img = popup.querySelector('.popup__photo');
+  img.src = card.offer.photos[0];
+  if (card.offer.photos.length > 1) {
+    for (i = 1; i < card.offer.photos.length; i++) {
+      var newImg = img.cloneNode(true);
+      photos.appendChild(newImg);
+      newImg.src = card.offer.photos[i];
+    };
+  }
+  return newImg;
+};
+addCardPhotos(notices[0]);
+
+var addCardType = function(card) {
+  var types = {
+    palace: 'Дворец',
+    flat: 'Квартира',
+    house: 'Дом',
+    bungalo: 'Бунгало'
+  };
+  popup.querySelector('.popup__type').textContent = types[card.offer.type];
+};
+addCardType(notices[0]);
+
+var addCardFeatures = function(card) {
+  var features = popup.querySelector('.popup__features');
+  if (!card.offer.features.includes('wifi')) {
+    features.removeChild(popup.querySelector('.popup__feature--wifi'));
+  }
+  if (!card.offer.features.includes('dishwasher')) {
+    features.removeChild(popup.querySelector('.popup__feature--dishwasher'));
+  }
+  if (!card.offer.features.includes('parking')) {
+    features.removeChild(popup.querySelector('.popup__feature--parking'));
+  }
+  if (!card.offer.features.includes('washer')) {
+    features.removeChild(popup.querySelector('.popup__feature--washer'));
+  }
+  if (!card.offer.features.includes('elevator')) {
+    features.removeChild(popup.querySelector('.popup__feature--elevator'));
+  }
+  if (!card.offer.features.includes('conditioner')) {
+    features.removeChild(popup.querySelector('.popup__feature--conditioner'));
+  }
+  return features;
+};
+addCardFeatures(notices[0]);
